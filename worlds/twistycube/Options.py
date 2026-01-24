bc6c64173bcd0270b7034351c576baa19675a886
spinerak <markvanderboor@hotmail.nl>
2026-01-24 23:00:39 +0100

    add min stickers option

--- worlds/twistycube/Options.py
+++ worlds/twistycube/Options.py
@@ -32,10 +32,23 @@ class RandomizeColorLayout(Toggle):
 
     display_name = "Randomize color layout"
     default = 0
+    
+class MinStickersToGoalOnSolve(Range):
+    """
+    The game will goal when the cube is solved, even if you don't have all stickers yet.
+    This option lets you set the minimum number of stickers you need to have collected for it to goal.
+    Setting it to 0 means you will goal as soon as you solve the cube.
+    Setting it to the max means you will have to wait until you have collected all stickers.
+    """
+
+    display_name = "Minimum stickers to goal on solve"
+    range_start = 0
+    range_end = 150
+    default = 0
 
 @dataclass
 class TwistyCubeOptions(PerGameCommonOptions):
     size_of_cube: SizeOfCube
     starting_stickers: StartingStickers
     randomize_color_layout: RandomizeColorLayout
-    
+    min_stickers_to_goal_on_solve: MinStickersToGoalOnSolve
